e77efa056ac14cf1c44cede1349cf68c44487167
srikanth157 <srikanthkodari157@gamil.com>
2021-08-31 22:22:32 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,7 @@
+const express=require("express")
+const mysql=require("mysql")
+const app=express()
+
+app.listen(8000,()=>{
+    console.log("server running")
+})
---
+++ front/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div >
+        <h1 class="heading">WELCOME TO BroChill</h1>
+        </div>
+     
+      <input type="text" class="textarea" placeholder="Enter image name"/>
+      <br/>
+      
+      <input type="file" class="fileinput" /><br/>
+      
+      <button class="btn uploadbtn">upload</button>
+     </div>
+    
+    
+  );
+}
+
+export default App;
