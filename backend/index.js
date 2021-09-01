4cf01d6fcbb1ad7d54f02e685e51799df72ac87b
srikanth157 <srikanthkodari157@gamil.com>
2021-09-01 20:42:13 +0530

    mysql table created with backend node js  and basic UI

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,72 @@
-const express=require("express")
-const mysql=require("mysql")
-const app=express()
+const express = require('express')
+const fileupload = require('express-fileupload')
+const cors = require('cors')
+const mysql=require('mysql')
 
-app.listen(8000,()=>{
-    console.log("server running")
+const app = express()
+const port = process.env.PORT || 4040;
+
+app.use(fileupload())
+app.use(cors())
+
+const pool = mysql.createPool({
+    host:'localhost',
+    user:"root",
+    password:'srikanth@157',
+    database:'brochill',
+    connectionLimit:10
+})
+
+app.get('/brohchil',(req,res)=>{
+    pool.getConnection((err,connection)=>{
+            if(err) throw err;
+            console.log('inside connnected')
+            connection.query('SELECT * FROM new_table ',(err,rows)=>{
+                connection.release();
+                if(!err){
+                    res.send({rows})
+                }else{
+                    console.log(err)
+                }
+            })
+        })
+})
+
+
+app.post('/upload', (req, res) =>
+{
+    let sampleFile;
+    let uploadPath;
+    if (!req.files || Object.keys(req.files).length === 0)
+    {
+        return res.send('please upload a file');
+    }
+    
+    sampleFile = req.files.file;
+    uploadPath=__dirname+'/public/images/'+sampleFile.name
+    
+    sampleFile.mv(uploadPath,function(err){
+        if(err) return res.send(err)
+        pool.getConnection((err,connection)=>{
+            if(err) throw err;
+            console.log('inside connnected')
+            connection.query('INSERT INTO new_table (img_src) VALUES (?)',[uploadPath],(err,result)=>{
+                connection.release();
+                if(!err){
+                    res.send('yes stored')
+                }else{
+                    console.log(err)
+                }
+            })
+        })
+       
+    })
+    
+})
+
+
+
+app.listen(port, () =>
+{
+    console.log('server running')
 })
--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,60 @@
+import React from 'react'
+import  axios,{ post } from 'axios';
 
-import './App.css';
+class App extends React.Component {
+state={file:null,imagesList:[],isImagesStored:false}
 
-function App() {
-  return (
-    <div className="App">
-      <div >
-        <h1 class="heading">WELCOME TO BroChill</h1>
-        </div>
-     
-      <input type="text" class="textarea" placeholder="Enter image name"/>
-      <br/>
+  onFormSubmit=(event)=>{
+    event.preventDefault()
+    const url = 'http://localhost:4040/upload';
+    const formData = new FormData();
+    formData.append('file', this.state.file);
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data'
+      }
+    }
+    post(url, formData, config)
+      .then((response) => {
+        console.log(response.data);
+      })
       
-      <input type="file" class="fileinput" /><br/>
-      
-      <button class="btn uploadbtn">upload</button>
-     </div>
-    
-    
-  );
+  }
+
+  onChange=(event)=> {
+    this.setState({ file: event.target.files[0]})
+  }
+
+  getImages=()=>{
+    const{imagesList}=this.state
+    axios.get("http://localhost:4040/brohchil").then((response)=>{
+      console.log(response.data.rows[0])
+      const data= response.data.rows[0]
+      this.setState({imagesList:[...imagesList,data]})
+    })
+  }
+
+  render() {
+    const {imagesList,isImagesStored}=this.state
+    console.log(imagesList)
+    return (
+      <>
+      <form onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input type="file" onChange={this.onChange}/>
+        <button type="submit">Upload</button>
+      </form>
+
+      <div>
+        <button type='button' onClick={this.getImages}>show images</button>
+        <ul>
+          {imagesList.map((each,key)=>(
+            <li><img src={`${each.img_src}`} /></li>
+          ))}
+        </ul>
+      </div>
+      </>
+   )
+  }
 }
-
-export default App;
+export default App
